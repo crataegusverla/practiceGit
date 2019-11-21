1c108acef30c8e876e0bf40ae054ac6d769a442d
Crataegus <urmambetovajam@gmail.com>
2019-11-21 17:00:10 +0600

    Created main with nums integer list

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(42,75,49,14,25);
+    }
+}
